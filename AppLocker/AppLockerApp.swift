c37e4bcd4a65d1ac6409117ab0b5e67d2f6cf49e
TranPhuong319 <duyphuong3a2@gmail.com>
2025-07-24 00:38:36 +0700

    Initial Commit

---
+++ AppLocker/AppLockerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppLockerApp.swift
+//  AppLocker
+//
+//  Created by Doe Phương on 24/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct AppLockerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
