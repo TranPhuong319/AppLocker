386f5ed7ec27831087f8d5f9c92c7de1ab6c74ee
TranPhuong319 <duyphuong3a2@gmail.com>
2025-08-26 21:45:22 +0700

    Create Settings View and Settings Window Controller

---
+++ AppLocker/Settings/SettingsView.swift
@@ -0,0 +1,35 @@
+//
+//  SettingsView.swift
+//  AppLocker
+//
+//  Created by Doe Phương on 26/8/25.
+//
+
+
+import SwiftUI
+import Sparkle
+
+struct SettingsView: View {
+    @State private var autoCheck = AppUpdater.shared.updaterController.updater.automaticallyChecksForUpdates
+    @State private var autoDownload = AppUpdater.shared.updaterController.updater.automaticallyDownloadsUpdates
+
+    var body: some View {
+        Form {
+            Toggle("Automatically check for updates.".localized, isOn: $autoCheck)
+                .onChange(of: autoCheck) { newValue in
+                    AppUpdater.shared.updaterController.updater.automaticallyChecksForUpdates = newValue
+                }
+
+            Toggle("Automatically download new updates.".localized, isOn: $autoDownload)
+                .onChange(of: autoDownload) { newValue in
+                    AppUpdater.shared.updaterController.updater.automaticallyDownloadsUpdates = newValue
+                }
+        }
+        .padding()
+        .frame(width: 300)
+    }
+}
+
+#Preview{
+    SettingsView()
+}
---
+++ AppLocker/Settings/SettingsWindowController.swift
@@ -0,0 +1,32 @@
+//
+//  SettingsWindowController.swift
+//  AppLocker
+//
+//  Created by Doe Phương on 26/8/25.
+//
+
+
+import SwiftUI
+
+class SettingsWindowController {
+    static let shared = SettingsWindowController()
+    
+    private var window: NSWindow?
+
+    func show() {
+        if window == nil {
+            let settingsView = SettingsView()
+            window = NSWindow(
+                contentRect: NSRect(x: 0, y: 0, width: 400, height: 300),
+                styleMask: [.titled, .closable, .miniaturizable],
+                backing: .buffered,
+                defer: false
+            )
+            window?.center()
+            window?.title = "Settings".localized
+            window?.contentView = NSHostingView(rootView: settingsView)
+        }
+        window?.makeKeyAndOrderFront(nil)
+        NSApp.activate(ignoringOtherApps: true)
+    }
+}
